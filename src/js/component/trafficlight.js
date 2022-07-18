4de8c7a9d0fa155833b2c32e95d25a461c0c197b
Mario <waro.brambilla@gmail.com>
2022-07-18 08:16:56 +0000

    finish

--- src/js/component/trafficlight.js
+++ src/js/component/trafficlight.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 export function Trafficlight() {
 	
 	const [selectedColor, setSelectedColor] = useState ("red");
+	const [selectedID, setID] = useState (" ");
 	
 	
 	return (
@@ -13,26 +14,23 @@ export function Trafficlight() {
 		<div 
 			onClick={() => setSelectedColor("red")}
 			className = {"light red" + " " +  ( (selectedColor === "red") ? "glow" :"")}
+			
 			>
 		</div>
 		<div 
 			onClick={() => setSelectedColor("yellow")}
 			className = {"light yellow" + " " + ( (selectedColor === "yellow") ? "glow" :"")}
+			
 			>
 		</div>
 		<div 
 			onClick={() => setSelectedColor("green")}
 			className = {"light green" + " " +  ( (selectedColor === "green") ? "glow" :"")}
+			
 			>
 		</div>
 	</div>
 	</div>
-	<div className ="controller d-flex justify-content-center">
-		<Button 
-		onClick={() => setBackground("purple")}
-		variant="primary"
-		id="purple">Change Color</Button>{' '}
-	</div >
 	</div>
 	);
 } 
